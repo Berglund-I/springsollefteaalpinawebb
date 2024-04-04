a6e3bfcf37a1ae26cba2caa05cda82ecca040554
Berglund-I <idbe2204@student.miun.se>
2024-04-04 17:05:13 +0200

    Initial commit

---
+++ src/main/java/org/example/springsollefteaalpinawebb/SpringsollefteaalpinawebbApplication.java
@@ -0,0 +1,13 @@
+package org.example.springsollefteaalpinawebb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsollefteaalpinawebbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsollefteaalpinawebbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springsollefteaalpinawebb/SpringsollefteaalpinawebbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springsollefteaalpinawebb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsollefteaalpinawebbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
