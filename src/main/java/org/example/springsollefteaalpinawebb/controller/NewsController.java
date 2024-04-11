cec8ff13c3804221fc7253913f7ff16937e951c1
Berglund-I <idbe2204@student.miun.se>
2024-04-11 09:44:27 +0200

    Connection with frontend and backend done

--- src/main/java/org/example/springsollefteaalpinawebb/controller/NewsController.java
+++ src/main/java/org/example/springsollefteaalpinawebb/controller/NewsController.java
@@ -3,12 +3,14 @@ package org.example.springsollefteaalpinawebb.controller;
 import org.example.springsollefteaalpinawebb.model.News;
 import org.example.springsollefteaalpinawebb.repository.NewsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("api/")
 public class NewsController {
